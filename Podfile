d5a98e56499657272fc2d9109423a1dea656652a
Bronts <bronts@163.com>
2016-12-25 23:35:57 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'RainbowWeather' do
+    pod 'Alamofire',  '~> 4.0'
+    pod 'SwifterSwift'
+    pod 'ReachabilitySwift', '~> 3' # https://github.com/ashleymills/Reachability.swift
+    pod 'RxSwift',    '~> 3.0'
+    pod 'RxCocoa',    '~> 3.0'
+    pod 'SQLite.swift', '~> 0.11.1'
+    pod 'SwiftyJSON'
+    #pod 'SDCycleScrollView','~> 1.65' # Banner轮播组件
+end
---
+++ RainbowWeather/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  RainbowWeather
+//
+//  Created by Yangzheng on 2016/12/25.
+//  Copyright © 2016年 Yangzheng. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
