d892ea7fecd75234ddf6a9b87e9ccd253a44478d
Bronts <bronts@163.com>
2016-12-30 21:27:44 +0800

    test for horizontal UICollectionView

--- RainbowWeather/MainViewController.swift
+++ RainbowWeather/MainViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let items = Observable.just([
+        1,3,5,6,7,8,1,23,34,5,6,
+        2,
+        3
+        ])
+    
+    // RxSwift资源回收包
+    private let disposeBag = DisposeBag()
+
+    let colors = [UIColor.red, UIColor.blue, UIColor.black]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        items
+            .bindTo(collectionView.rx.items) { (collectionView, row, element) in
+                let indexPath = IndexPath(row: row, section: 0)
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as UICollectionViewCell// as! NumberCell
+                cell.backgroundColor = self.colors[row % 3]//"\(element) @ \(row)"
+                return cell
+            }
+            .addDisposableTo(disposeBag)
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
